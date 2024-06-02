ef8353c45b6ba1afb05b2e3f8c1f10062f0d3317
tasyaangesti <tasyahestiaangesti@gmail.com>
2024-06-02 22:10:09 +0700

    fixing reponsive

--- src/Page/ContactUs.jsx
+++ src/Page/ContactUs.jsx
@@ -1,94 +1,90 @@
 export function ContactUs() {
   return (
-    <div>
-      <div className="relative min-h-screen">
-        {/* Hubungi Kami */}
-        <div className="text-center m-14">
-          <h1 className="font-bold text-[#26355D] text-2xl">HUBUNGI KAMI</h1>
-          <p className="mx-auto max-w-2xl mt-2.5 text-base">
-            Selamat datang di halaman Hubungi Kami! Jika Anda memiliki
-            pertanyaan, masukan, jangan ragu untuk menghubungi kami. Tim kami
-            siap membantu Anda dengan segala yang Anda perlukan.
+    <div className="relative min-h-screen">
+      {/* Hubungi Kami */}
+      <div className="text-center m-4 md:m-14">
+        <h1 className="font-bold text-[#26355D] text-2xl">HUBUNGI KAMI</h1>
+        <p className="mx-auto max-w-2xl mt-2.5 text-base">
+          Selamat datang di halaman Hubungi Kami! Jika Anda memiliki pertanyaan,
+          masukan, jangan ragu untuk menghubungi kami. Tim kami siap membantu
+          Anda dengan segala yang Anda perlukan.
+        </p>
+      </div>
+
+      {/* Background */}
+      <div className="relative w-full mt-3">
+        <img
+          src="/public/assets/city.png"
+          alt="city"
+          className="w-full h-auto"
+        />
+        {/* Send Us a Message */}
+        <div className="absolute top-0 left-0 w-full h-full flex items-center md:ml-32 -mt-52">
+          <h1 className="text-white text-4xl font-bold">Send Us a Message</h1>
+        </div>
+      </div>
+
+      {/* Layout Form and Main Office */}
+      <div className="absolute top-[300px] w-full max-w-6xl p-6 mx-auto flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-6 md:ml-24">
+        {/* Form */}
+        <div className="w-full md:w-1/2">
+          <form className="space-y-4">
+            <div>
+              <textarea
+                id="name"
+                name="name"
+                placeholder="Your Name"
+                className="mt-1 block w-full rounded-md bg-white bg-opacity-30 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm pl-4 pt-2 h-10"
+              />
+            </div>
+            <div>
+              <textarea
+                id="email"
+                name="email"
+                placeholder="Your Email"
+                className="mt-1 block w-full rounded-md bg-white bg-opacity-30 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm pl-4 pt-2 h-10"
+              />
+            </div>
+            <div>
+              <textarea
+                id="message"
+                name="message"
+                placeholder="Your Message"
+                className="mt-1 block w-full h-[165px] rounded-md bg-white bg-opacity-30 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm pl-4 pt-2"
+              />
+            </div>
+            <div className="text-center">
+              <button
+                type="submit"
+                className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-3xl text-white bg-white bg-opacity-30 hover:bg-slate-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 ml-[400px]"
+              >
+                Send a Message
+              </button>
+            </div>
+          </form>
+        </div>
+
+        {/* Main Office Details */}
+        <div className="w-full md:w-1/2 h-[110%] mt-4 md:mt-0 bg-white bg-opacity-30 rounded-lg shadow-lg p-6">
+          <h2 className="text-2xl font-bold text-white">Main Office</h2>
+          <p className="text-base text-white">
+            Travelindo Official
+            <br />
+            Jl. Bunga Wijaya Kesuma No.09, Padang Bulan, Kec. Medan Selayang,
+            Kota Medan, Sumatera Utara 20131
           </p>
-        </div>
-
-        {/* Background */}
-        <div className="relative w-full mt-3">
-          <img
-            src="/public/assets/city.png"
-            alt="city"
-            className="w-full h-auto"
-          />
-          {/* Send Us a Message */}
-          <div className="absolute top-0 left-0 w-full h-full flex items-center ml-32 -mt-52">
-            <h1 className="text-white text-4xl font-bold">Send Us a Message</h1>
-          </div>
-        </div>
-
-        {/* Layout Form and Main Office */}
-        <div className="absolute top-1/2 -translate-y-24 w-full max-w-6xl p-6 mx-auto flex space-x-6 ml-24">
-          {/* Form */}
-          <div className="w-1/2">
-            <form className="space-y-4">
-              <div>
-                <textarea
-                  id="name"
-                  name="name"
-                  placeholder="Your Name"
-                  className="mt-1 block w-full rounded-md bg-white bg-opacity-30 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm pl-4 pt-2"
-                />
-              </div>
-              <div>
-                <textarea
-                  id="email"
-                  name="email"
-                  placeholder="Your Email"
-                  className="mt-1 block w-full rounded-md bg-white bg-opacity-30 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm pl-4 pt-2"
-                />
-              </div>
-              <div>
-                <textarea
-                  id="message"
-                  name="message"
-                  placeholder="Your Message"
-                  className="mt-1 block w-full h-[150px] rounded-md bg-white bg-opacity-30 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm pl-4 pt-2"
-                />
-              </div>
-              <div className="text-center">
-                <button
-                  type="submit"
-                  className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-3xl text-white bg-white bg-opacity-30 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 ml-96"
-                >
-                  Send a Message
-                </button>
-              </div>
-            </form>
-          </div>
-
-          {/* Main Office Details */}
-          <div className="w-1/2 h-[110%] bg-white bg-opacity-30 rounded-lg shadow-lg p-6">
-            <h2 className="text-2xl font-bold text-white">Main Office</h2>
-            <p className="text-base text-white">
-              Travelindo Official
-              <br />
-              Jl. Bunga Wijaya Kesuma No.09, Padang Bulan, Kec. Medan Selayang,
-              Kota Medan, Sumatera Utara 20131
-            </p>
-            <h2 className="text-2xl font-bold text-white mt-6">
-              Second Office
-            </h2>
-            <p className="text-base text-white">
-              Travelindo Official
-              <br />
-              Jl. Bunga Wijaya Kesuma No.09, Padang Bulan, Kec. Medan Selayang,
-              Kota Medan, Sumatera Utara 20131
-            </p>
-          </div>
+          <h2 className="text-2xl font-bold text-white mt-6">Second Office</h2>
+          <p className="text-base text-white">
+            Travelindo Official
+            <br />
+            Jl. Bunga Wijaya Kesuma No.09, Padang Bulan, Kec. Medan Selayang,
+            Kota Medan, Sumatera Utara 20131
+          </p>
         </div>
       </div>
 
       {/* temukan kami disini */}
-      <div className="text-center m-14">
+      <div className="text-center m-4 md:m-14">
         <h1 className="font-bold text-[#26355D] text-2xl">
           TEMUKAN KAMI DISINI
         </h1>
